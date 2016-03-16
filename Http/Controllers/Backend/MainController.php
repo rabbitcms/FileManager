f2ad29551753412d72fffdb8a05928ec7df426c2
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2016-03-17 01:49:17 +0200

    broken controller

---
+++ Http/Controllers/Backend/MainController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace RabbitCMS\FileManager\Http\Controllers\Backend;
+
+use RabbitCMS\FileManager\Http\Controllers\Controller;
+use RabbitCMS\Backend\Support\Metronic;
+
+class MainController extends Controller
+{
+
+    public function index()
+    {
+        Metronic::addPath(trans('Home'), '/');
+        Metronic::addPath(trans('FileManager'), null);
+        return $this->view('backend.main')
+            ->with($this->getViewVars());
+    }
+
+    protected function getViewVars()
+    {
+        $dir = '/bower/elfinder';
+        $locale = str_replace('-', '_', app()->getLocale());
+        if (!file_exists(public_path("{$dir}/js/i18n/elfinder.{$locale}.js"))) {
+            $locale = false;
+        }
+        $csrf = true;
+
+        return compact('dir', 'locale', 'csrf');
+    }
+
+}
